d747a5a80260573cc03b6ffc748dca61793842bd
Carl Jayson Buñi <cabuni.itd.pps@gmail.com>
2021-01-06 16:42:47 +0800

    Removing Commend

--- src/TodoApp.API/TodoApp.API/Services/Commands/Todos/Create/CreateTodoService.cs
+++ src/TodoApp.API/TodoApp.API/Services/Commands/Todos/Create/CreateTodoService.cs
@@ -10,7 +10,7 @@ namespace TodoApp.API.Services.Commands.Todos.Create
 {
     public class CreateTodoService : IRequest<TodoReadDto>
     {
-        //public int Id { get; set; }
+        
         [Required]
         public string Name { get; set; }
         [Required]

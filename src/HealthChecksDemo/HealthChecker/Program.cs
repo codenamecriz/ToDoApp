1d032f188145eefe4da7e38f5ec84292213c41c8
Carl Jayson Buñi <cabuni.itd.pps@gmail.com>
2021-02-10 10:39:39 +0800

    comment

--- src/HealthChecksDemo/HealthChecker/Program.cs
+++ src/HealthChecksDemo/HealthChecker/Program.cs
@@ -20,7 +20,7 @@ namespace HealthChecker
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>();//
                 });
     }
 }
